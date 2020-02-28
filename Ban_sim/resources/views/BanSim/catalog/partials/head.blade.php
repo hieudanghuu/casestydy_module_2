af7472c5a6ce0578aa317bf5c2e2b72fa17a0769
hieudanghuu <hieudanghuu08091989@gmail.com>
2020-02-28 16:19:55 +0700

    update

--- Ban_sim/resources/views/BanSim/catalog/partials/head.blade.php
+++ Ban_sim/resources/views/BanSim/catalog/partials/head.blade.php
@@ -6,23 +6,23 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
 
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/open-iconic-bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/minishop/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/animate.css')}}">
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/minishop/css/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/minishop/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/magnific-popup.css')}}">
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/aos.css')}}">
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/ionicons.min.css')}}">
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/bootstrap-datepicker.css')}}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{asset('/minishop/css/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/jquery.timepicker.css')}}">
 
 
-    <link rel="stylesheet" href="{{asset('/minishop/css/flaticon.css')}}">
-    <link rel="stylesheet" href="{{asset('/minishop/css/icomoon.css')}}">
-    <link rel="stylesheet" href="{{asset('/minishop/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('minishop/css/style.css')}}">
 </head>
--- Ban_sim/resources/views/BanSim/catalog/partials/js.blade.php
+++ Ban_sim/resources/views/BanSim/catalog/partials/js.blade.php
@@ -1,20 +1,20 @@
 
-<script src="{{asset('/minishop/js/jquery.min.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery-migrate-3.0.1.min.js')}}"></script>
-<script src="{{asset('/minishop/js/popper.min.js')}}"></script>
-<script src="{{asset('/minishop/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery.easing.1.3.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery.waypoints.min.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery.stellar.min.js')}}"></script>
-<script src="{{asset('/minishop/js/owl.carousel.min.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('/minishop/js/aos.js')}}"></script>
-<script src="{{asset('/minishop/js/jquery.animateNumber.min.js')}}"></script>
-<script src="{{asset('/minishop/js/bootstrap-datepicker.js')}}"></script>
-<script src="{{asset('/minishop/js/scrollax.min.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.min.js')}}"></script>
+<script src="{{asset('minishop/js/jquery-migrate-3.0.1.min.js')}}"></script>
+<script src="{{asset('minishop/js/popper.min.js')}}"></script>
+<script src="{{asset('minishop/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.easing.1.3.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.stellar.min.js')}}"></script>
+<script src="{{asset('minishop/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('minishop/js/aos.js')}}"></script>
+<script src="{{asset('minishop/js/jquery.animateNumber.min.js')}}"></script>
+<script src="{{asset('minishop/js/bootstrap-datepicker.js')}}"></script>
+<script src="{{asset('minishop/js/scrollax.min.js')}}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-<script src="{{asset('/minishop/js/google-map.js')}}"></script>
-<script src="{{asset('/minishop/js/main.js')}}"></script>
+<script src="{{asset('minishop/js/google-map.js')}}"></script>
+<script src="{{asset('minishop/js/main.js')}}"></script>
 <script src="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
 {{--<script>--}}
 {{--    $(document).ready(function () {--}}
